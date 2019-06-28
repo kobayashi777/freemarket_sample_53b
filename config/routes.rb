576bf025247ab0e7f6cbf71d091883c4a83f886f
pegchose <j.c.lsevp9@gmail.com>
2019-06-28 19:18:41 +0900

    結合ok　商品詳細除く

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
   resources :products, only: [:index, :show, :new]
-  resources :users, only: [:index, :show, :new, :edit]
+  resources :users, only: [:index, :show, :new, :edit, :credit]
   resources :login, only: :index
   resources :signup, only: :index
   resources :registration1, only: [:index, :show, :new]
