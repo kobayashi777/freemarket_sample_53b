7d85c1bd3f83d8dcf933fadd70efce693abcd637
kobayashi <kortaban@gmail.com>
2019-06-29 13:16:54 +0900

    product table 作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_29_030118) do
+ActiveRecord::Schema.define(version: 2019_06_29_041250) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -28,5 +28,36 @@ ActiveRecord::Schema.define(version: 2019_06_29_030118) do
     t.index ["name"], name: "index_categories_on_name"
   end
 
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "product_name", null: false
+    t.text "product_introduction"
+    t.bigint "category_id"
+    t.bigint "products_size_id"
+    t.bigint "brand_id"
+    t.integer "product_status", default: 0, null: false
+    t.integer "delivery_charge", default: 0, null: false
+    t.integer "delivery_area", default: 0, null: false
+    t.string "price", null: false
+    t.integer "delivery_days", default: 0, null: false
+    t.integer "delivery_method", default: 0, null: false
+    t.integer "trading_state", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["brand_id"], name: "fk_rails_f3b4d49caa"
+    t.index ["category_id"], name: "fk_rails_fb915499a4"
+    t.index ["product_name"], name: "index_products_on_product_name"
+    t.index ["products_size_id"], name: "fk_rails_9c56f738b7"
+  end
+
+  create_table "products_sizes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "size", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["size"], name: "index_products_sizes_on_size"
+  end
+
   add_foreign_key "brands", "categories"
+  add_foreign_key "products", "brands"
+  add_foreign_key "products", "categories"
+  add_foreign_key "products", "products_sizes"
 end
