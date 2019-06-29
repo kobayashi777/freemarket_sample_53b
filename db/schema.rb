93838a5826082663c8598af9d50a59c573911ed5
kobayashi <kortaban@gmail.com>
2019-06-29 12:09:04 +0900

    brand table 作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_28_110950) do
+ActiveRecord::Schema.define(version: 2019_06_29_030118) do
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_brands_on_category_id"
+    t.index ["name"], name: "index_brands_on_name"
+  end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -19,4 +28,5 @@ ActiveRecord::Schema.define(version: 2019_06_28_110950) do
     t.index ["name"], name: "index_categories_on_name"
   end
 
+  add_foreign_key "brands", "categories"
 end
