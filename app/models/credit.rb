ccae59029a97a3e2c388ab1ca44bcc3414f53976
AtoraYamada <atora2018@gmail.com>
2019-07-02 16:28:12 +0900

    set validates for user.rb and credit.rb

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,10 +1,14 @@
 class Credit < ApplicationRecord
   enum validated_date_year: {
-    "--":0,"19":19,"20":20,"21":21,"22":22,"23":23,"24":24,"25":26
+    "19":19,"20":20,"21":21,"22":22,"23":23,"24":24,"25":26
   }, _suffix: true
   enum validated_date_month: {
-    "--":0,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"11":11,"12":12
+    "1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"11":11,"12":12
   }, _suffix: true
 
   belongs_to :user, optional: true
+  VALID_CARD_NUMBER_REGEX = /\A\d{14,16}\z/   
+  VALID_SECRET_CODE_REGEX = /\A\d{3,4}\z/ 
+  validates :card_number, presence: true, format: { with: VALID_CARD_NUMBER_REGEX }
+  validates :security_code, presence: true, format: { with: VALID_SECRET_CODE_REGEX }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,27 @@ class User < ApplicationRecord
   
   has_one :credit
   accepts_nested_attributes_for :credit
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+  VALID_ADDRESS_NUMBER_REGEX = /\A[0-9]{3}-[0-9]{4}\z/
+  validates :nickname, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: {minimum: 6, maximum: 10}
+  validates :password_confirmation, presence: true, length: {minimum: 6, maximum: 10}
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthdate_year, numericality: true
+  validates :birthdate_month, numericality: true
+  validates :birthdate_day, numericality: true
+  validates :phone_number,  presence: true, format: { with: VALID_PHONE_REGEX }
+  validates :address_last_name, presence: true
+  validates :address_first_name, presence: true
+  validates :address_last_name_kana, presence: true
+  validates :address_first_name_kana, presence: true
+  validates :address_number, presence: true, format: { with: VALID_ADDRESS_NUMBER_REGEX }
+  validates :address_name, presence: true
+  validates :address_block, presence: true
 end
