444fcfa500b20916f268ec58c38ad52dd44f4b5a
ooshima manami <maachan.s2.0622@gmail.com>
2019-07-02 11:55:57 +0900

    seed categoli list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_29_041250) do
+ActiveRecord::Schema.define(version: 2019_07_01_030311) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2019_06_29_041250) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
     t.index ["name"], name: "index_categories_on_name"
   end
 
