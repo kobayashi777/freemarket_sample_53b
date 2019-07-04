1014b49777c57ab366c7544dafd164b63f47be61
AtoraYamada <atora2018@gmail.com>
2019-07-04 21:12:53 +0900

    set flexible size selectbox

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,74 +1,137 @@
 $(function(){
   function appendOption(category){
-    var html = `<option value="${category.name}">${category.name}</option>`;
+    var html = `<option value="${category.name}" data-category="${category.id}">${category.name}</option>`;
+    return html;
+  }
+  function appendSizeOption(size){
+    var html = `<option value="${size.size}">${size.size}</option>`;
     return html;
   }
   function appendChidrenBox(insertHTML){
     var childSelectHtml = '';
     childSelectHtml = `<div class='listing-select-wrapper__added' id= 'children_wrapper'>
                         <div class='listing-select-wrapper__box'>
-                        <select class="listing-select-wrapper__box--select" id="child_category" name="prefecture">
-                          <option value="---">---</option>
-                          ${insertHTML}
-                        <select>
-                        <i class='fas fa-chevron-down listing-select-wrapper__box--arrow-down'></i>`;
+                          <select class="listing-select-wrapper__box--select" id="child_category" name="prefecture">
+                            <option value="---" data-category="---">---</option>
+                            ${insertHTML}
+                          <select>
+                          <i class='fas fa-chevron-down listing-select-wrapper__box--arrow-down'></i>
+                        </div>
+                      </div>`;
     $('.listing-product-detail__category').append(childSelectHtml);
   }
   function appendGrandchidrenBox(insertHTML){
     var grandchildSelectHtml = '';
     grandchildSelectHtml = `<div class='listing-select-wrapper__added' id= 'grandchildren_wrapper'>
                               <div class='listing-select-wrapper__box'>
-                              <select class="listing-select-wrapper__box--select" id="grandchild_category" name="prefecture">
-                                <option value="---">---</option>
-                                ${insertHTML}
-                              <select>
-                              <i class='fas fa-chevron-down listing-select-wrapper__box--arrow-down'></i>`;
+                                <select class="listing-select-wrapper__box--select" id="grandchild_category" name="prefecture">
+                                  <option value="---" data-category="---">---</option>
+                                  ${insertHTML}
+                                <select>
+                                <i class='fas fa-chevron-down listing-select-wrapper__box--arrow-down'></i>
+                              </div>
+                            </div>`;
     $('.listing-product-detail__category').append(grandchildSelectHtml);
+  }
+  function appendSizeBox(insertHTML){
+    var sizeSelectHtml = '';
+    sizeSelectHtml = `<div class="listing-product-detail__size" id= 'size_wrapper'>
+                        <label class="listing-default__label" for="サイズ">サイズ</label>
+                        <span class='listing-default--require'>必須</span>
+                        <div class='listing-select-wrapper__added--size'>
+                          <div class='listing-select-wrapper__box'>
+                            <select class="listing-select-wrapper__box--select" id="size" name="prefecture">
+                              <option value="---">---</option>
+                              ${insertHTML}
+                            <select>
+                            <i class='fas fa-chevron-down listing-select-wrapper__box--arrow-down'></i>
+                          </div>
+                        </div>
+                      </div>`;
+    $('.listing-product-detail__category').append(sizeSelectHtml);
   }
   $('#parent_category').on('change', function(){
     var parentCategory = document.getElementById('parent_category').value;
-    $.ajax({
-      url: 'get_category_children',
-      type: 'GET',
-      data: { parent_name: parentCategory },
-      dataType: 'json'
-    })
-    .done(function(children){
+    if (parentCategory != "---"){
+      $.ajax({
+        url: 'get_category_children',
+        type: 'GET',
+        data: { parent_name: parentCategory },
+        dataType: 'json'
+      })
+      .done(function(children){
+        $('#children_wrapper').remove();
+        $('#grandchildren_wrapper').remove();
+        $('#size_wrapper').remove();
+        var insertHTML = '';
+        children.forEach(function(child){
+          insertHTML += appendOption(child);
+        });
+        appendChidrenBox(insertHTML);
+      })
+      .fail(function(){
+        alert('カテゴリー取得に失敗しました');
+      })
+    }else{
       $('#children_wrapper').remove();
       $('#grandchildren_wrapper').remove();
-      var insertHTML = '';
-      children.forEach(function(child){
-        insertHTML += appendOption(child);
-      });
-      appendChidrenBox(insertHTML);
-    })
-    .fail(function(){
-      $('#children_wrapper').remove();
-      $('#grandchildren_wrapper').remove();
-    })
+      $('#size_wrapper').remove();
+    }
   });
 
   $('.listing-product-detail__category').on('change', '#child_category', function(){
-    var childCategory = document.getElementById('child_category').value;
-    console.log(childCategory);
-    $.ajax({
-      url: 'get_category_grandchildren',
-      type: 'GET',
-      data: { child_name: childCategory },
-      dataType: 'json'
-    })
-    .done(function(grandchildren){
-      if (grandchildren.length != 0) {
-        $('#grandchildren_wrapper').remove();
-        var insertHTML = '';
-        grandchildren.forEach(function(grandchild){
-          insertHTML += appendOption(grandchild);
-        });
-        appendGrandchidrenBox(insertHTML);
-      }
-    })
-    .fail(function(){
+    var childId = $('#child_category option:selected').data('category');
+    if (childId != "---"){
+      $.ajax({
+        url: 'get_category_grandchildren',
+        type: 'GET',
+        data: { child_id: childId },
+        dataType: 'json'
+      })
+      .done(function(grandchildren){
+        if (grandchildren.length != 0) {
+          $('#grandchildren_wrapper').remove();
+          $('#size_wrapper').remove();
+          var insertHTML = '';
+          grandchildren.forEach(function(grandchild){
+            insertHTML += appendOption(grandchild);
+          });
+          appendGrandchidrenBox(insertHTML);
+        }
+      })
+      .fail(function(){
+        alert('カテゴリー取得に失敗しました');
+      })
+    }else{
       $('#grandchildren_wrapper').remove();
-    })
+      $('#size_wrapper').remove();
+    }
+  });
+
+  $('.listing-product-detail__category').on('change', '#grandchild_category', function(){
+    var grandchildId = $('#grandchild_category option:selected').data('category');
+    if (grandchildId != "---"){
+      $.ajax({
+        url: 'get_size',
+        type: 'GET',
+        data: { grandchild_id: grandchildId },
+        dataType: 'json'
+      })
+      .done(function(sizes){
+        $('#size_wrapper').remove();
+        if (sizes.length != 0) {
+        var insertHTML = '';
+          sizes.forEach(function(size){
+            insertHTML += appendSizeOption(size);
+          });
+          appendSizeBox(insertHTML);
+        }
+      })
+      .fail(function(){
+        alert('サイズ取得に失敗しました');
+      })
+    }else{
+      $('#size_wrapper').remove();
+    }
   });
 });
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,10 +24,22 @@ class ProductsController < ApplicationController
    end
 
    def get_category_children
-      @category_children = Category.find_by(name: "#{params[:parent_name]}").children
+      @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
    end
 
    def get_category_grandchildren
-      @category_grandchildren = Category.find_by(name: "#{params[:child_name]}").children
+      @category_grandchildren = Category.find("#{params[:child_id]}").children
+   end
+
+   def get_size
+      selected_grandchild = Category.find("#{params[:grandchild_id]}")
+      if related_size_parent = selected_grandchild.products_sizes[0]
+         @sizes = related_size_parent.children
+      else
+         selected_child = Category.find("#{params[:grandchild_id]}").parent
+         if related_size_parent = selected_child.products_sizes[0]
+            @sizes = related_size_parent.children
+         end
+      end
    end
 end
