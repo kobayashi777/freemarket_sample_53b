c4881fcab881d08e27e579119db73323f8f41ef4
pegchose <j.c.lsevp9@gmail.com>
2019-07-10 14:39:11 +0900

    TOP商品各info（exhibitor/product_statusを除く）
    不要なコメントも削除(未使用クラス名）

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
 
    def show
     @product = Product.find(params[:id])
-    binding.pry
+    #binding.pry
    end
    
    # TODO:画像投稿機能のS3設定と本番環境での分岐
