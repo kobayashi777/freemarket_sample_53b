a0b96b0111213325cb6539646465bf866b3dfe7a
nagisa-miura <nagisaue@icloud.com>
2019-07-11 15:36:05 +0900

    unicorn.rbの設定をchatspaceと同じに

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,36 +1,56 @@
-
 app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
-# currentを指定
-working_directory "#{app_path}/current"
 
-# それぞれ、sharedの中を参照するよう変更
+working_directory "#{app_path}/current"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
-# 再起動時に古いプロセスを削除
-# before_fork do |server, worker|
-#   defined?(ActiveRecord::Base) &&
-#     ActiveRecord::Base.connection.disconnect!
+listen 3000
+timeout 60
 
-#   if run_once
-#     run_once = false # prevent from firing again
-#   end
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 
-#   old_pid = "#{server.config[:pid]}.oldbin"
-#   if File.exist?(old_pid) && server.pid != old_pid
-#     begin
-#       sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-#       Process.kill(sig, File.read(old_pid).to_i)
-#     rescue Errno::ENOENT, Errno::ESRCH => e
-#       logger.error e
-#     end
-#   end
-# end
+check_client_connection false
 
-# after_fork do |_server, _worker|
-#   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
-# end
+run_once = true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+
+  if run_once
+    run_once = false # prevent from firing again
+  end
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exist?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH => e
+      logger.error e
+    end
+  end
+end
+
+after_fork do |_server, _worker|
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+end
+
+
+
+# app_path = File.expand_path('../../../', __FILE__)
+
+# worker_processes 1
+# # currentを指定
+# working_directory "#{app_path}/current"
+
+# # それぞれ、sharedの中を参照するよう変更
+# listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+# pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+# stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+# stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
