b6ee92dc43b28b8ef11736f7569b5ae8f7cab36d
nagisa-miura <nagisaue@icloud.com>
2019-07-11 19:06:44 +0900

    basic認証暫定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action , if Rails.env.production?
-  # before_action :basic_auth, if: :production?
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
