336b2634f299b08e25dc7fca2b5cecfde9febfa9
AtoraYamada <atora2018@gmail.com>
2019-07-11 13:06:07 +0900

    add on('turbolinks:load' to JS

--- app/assets/javascripts/edit_products.js
+++ app/assets/javascripts/edit_products.js
@@ -1,26 +1,27 @@
-$(function(){
-  var count_images = $('.image-box').length
-  if (count_images == 10){
-    $('.listing-upload__drag-box').hide();
-  } 
-  $('.image-box__bottom--delete').on('click', function(){
-    $(this).parents('.image-box').hide();
-    count_images = $('.image-box:visible').length
-    console.log(count_images)
-    if (count_images < 10){
-      $('.listing-upload__drag-box').show();
-    }
-  })
-  $('.listing-product-submit-box__send-btn').on('click', function(e){
-    e.preventDefault();
-    deletePhotoIds = [];
-    $('.image-box:hidden').each(function(index, photo){
-      deletePhotoIds.push($(photo).data('photo'))
+$(document).on('turbolinks:load', function() { 
+  $(function(){
+    var count_images = $('.image-box').length
+    if (count_images == 10){
+      $('.listing-upload__drag-box').hide();
+    } 
+    $('.image-box__bottom--delete').on('click', function(){
+      $(this).parents('.image-box').hide();
+      count_images = $('.image-box:visible').length
+      console.log(count_images)
+      if (count_images < 10){
+        $('.listing-upload__drag-box').show();
+      }
     })
-    console.log(deletePhotoIds)
-    $("#delete-photos").append(
-      $('<input type="hidden" name="delete_photos">').val(deletePhotoIds)
-    ); 
-    $('#editForm').submit();
+    $('.listing-product-submit-box__send-btn').on('click', function(e){
+      e.preventDefault();
+      deletePhotoIds = [];
+      $('.image-box:hidden').each(function(index, photo){
+        deletePhotoIds.push($(photo).data('photo'))
+      })
+      $("#delete-photos").append(
+        $('<input type="hidden" name="delete_photos">').val(deletePhotoIds)
+      ); 
+      $('#productForm').submit();
+    })
   })
-})
+});
