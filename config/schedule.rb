3857f9830283a489d9cb96f609a07c8351c6a0f1
nagisa-miura <nagisaue@icloud.com>
2019-07-13 12:25:39 +0900

    定期処理ログファイル指定

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + "/environment")
-set :output, 'log/crontab.log'
+set :output, '/var/www/freemarket_sample_53b/shared/log/crontab.log'
+
+every 1.hours do
+  command "echo 'hello, whenever world!'"
+end
 
 every 1.day at: '11:00 am' do
   runner "ActiveStorage::Blob.unattached.find_each(&:purge_later)"
