87ec0a832b092b2a1108bcf8b830aa8e7f02de26
nagisa-miura <nagisaue@icloud.com>
2019-07-13 18:55:29 +0900

    TOPカルーセルのturbolinks修正

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,13 +1,15 @@
-$(function(){
-  $('.slick').slick({
-    // autoplay:true,
-    speed: 1000,
-    dots:true,
-    pauseOnHover: true,
+$(document).on('turbolinks:load', function() {
+  $(function(){
+    $('.slick').slick({
+      // autoplay:true,
+      speed: 1000,
+      dots:true,
+      pauseOnHover: true,
+    });
+    $( '.slick' ).on( 'mouseenter', 'li', function() {
+      $( this ).click();
+      } );
   });
-  $( '.slick' ).on( 'mouseenter', 'li', function() {
-    $( this ).click();
-    } );
 });
 
 
