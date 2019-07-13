ad8384b0530a3b6a8db90f30e194d02e7ce84af1
nagisa-miura <nagisaue@icloud.com>
2019-07-13 12:42:48 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,4 +38,3 @@ end
 # wheneverの本番環境反映
 set :whenever_command, "bundle exec whenever"
 require "whenever/capistrano"
-job_type :rbenv_rake, %q!eval "$(rbenv init -)"; cd :path && :environment_variable=:environment bundle exec rake :task --silent :output!
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 set :output, '/var/www/freemarket_sample_53b/shared/log/crontab.log'
 env :PATH, ENV['PATH'] 
+job_type :rbenv_rake, %q!eval "$(rbenv init -)"; cd :path && :environment_variable=:environment bundle exec rake :task --silent :output!
 
 every 1.hours do
   command "echo 'hello, whenever world!'"
