de7aaf2bd5f62b5d6dbf303622e0e5f29e4325b3
kobayashi <kortaban@gmail.com>
2019-07-14 12:19:23 +0900

    エラー画面　実装

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -8,18 +8,21 @@ class PurchaseController < ApplicationController
       #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "card", action: "new"
     else
-      Payjp.api_key = Rails.application.credentials.PAYJP_PRIVATE_KEY
+      # Payjp.api_key = Rails.application.credentials.PAYJP_PRIVATE_KEY
+      Payjp.api_key = "sk_test_8a22f3d6cc423679a2d3e948"
       #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(card.customer_id)
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
       @default_card_information = customer.cards.retrieve(card.card_id)
       @card_brand = card_brand
     end
+    @product = Product.all
   end
 
   def pay
     card = Card.where(user_id: current_user.id).first
-    Payjp.api_key = Rails.application.credentials.PAYJP_PRIVATE_KEY
+    # Payjp.api_key = Rails.application.credentials.PAYJP_PRIVATE_KEY
+    Payjp.api_key = "sk_test_8a22f3d6cc423679a2d3e948"
     Payjp::Charge.create(
     :amount => 135000, #支払金額を入力（itemテーブル等に紐づけても良い）
     :customer => card.customer_id, #顧客ID
@@ -35,6 +38,9 @@ class PurchaseController < ApplicationController
     @card_brand = card_brand
   end
 
+  def update
+    product = Product.find(params[:id])
+  end
 
   def card_brand
     case @default_card_information.brand
