de7fcadb5011ff571f46a5b660170389551e4ea1
AtoraYamada <atora2018@gmail.com>
2019-07-15 14:41:37 +0900

    add commenst to photos.js

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,13 +1,16 @@
 $(document).on('turbolinks:load', function() { 
   $(function(){
+    // 画像向きをそのままでimgタグ作成
     function buildImgHtml(image){
       var img = `<img src='${image}' class='image-box__top--image' width='114' height='114'>`
       return img
     }
+    // 画像向きを90度回転させてimgタグ作成
     function buildRotateImgHtml(image){
       var img = `<img src='${image}' class='image-box__top--image' width='114' height='114' style="transform: rotate(90deg);">`
       return img
     }
+    // プレビュー用のhtml作成
     function appendImageBox(insertImg){
       var html =
       `<div class='image-box-new'>
@@ -19,31 +22,34 @@ $(document).on('turbolinks:load', function() {
       </div>`
       $('.listing-upload__drag-box').before(html);
     }
+    // file_fieldに画像がアップロードされた時
     $('#photo_upload').change(function(e){
       e.preventDefault();
-      var files = e.target.files;
-      var count_images = $('.image-box:visible').length;
-      if (count_images+files.length > 10){
+      var files = e.target.files; // アップロードされたファイルを取得
+      var count_images = $('.image-box:visible').length; // 初期で表示されている画像の枚数をカウント
+      if (count_images+files.length > 10){ // 初期で表示されている画像とアップロードされた画像の枚数が10枚を超える場合
+        // エラー表示
         var errorHtml = 
         `<ul id='upload-error'>
           <li class= 'has_error_text'>画像は合計1枚以上10枚以下で選択してください</li>
         </ul>`
         $('#upload-error').remove();
         $('.listing-upload').append(errorHtml);
-        $('html,body').scrollTop(0);
-        $('#photo_upload').val("");
+        $('html,body').scrollTop(0); // ページトップへスクロール
+        $('#photo_upload').val(""); // アップロードされた中身をリセット
       } else {
-        $('.image-box-new').remove();
+        $('.image-box-new').remove(); // 元々アップロードされた画像のプレビューを削除
         $.each(files, function(index, file){
+          // アップロードされた画像を１つづつ取り出し、画像のorientationを取得
           EXIF.getData(file, function() {
             var orientation = "";
             orientation = EXIF.getTag(this, "Orientation")
             var fileReader = new FileReader();
             fileReader.onload = function(e) {
-              var loadedImageUri = e.target.result;
-              if ( orientation == 6 ){
+              var loadedImageUri = e.target.result; // 画像のURIを取得
+              if ( orientation == 6 ){ //縦画像の場合
                 insertImg = buildRotateImgHtml(loadedImageUri);
-              } else {
+              } else { // 横画像の場合
                 insertImg = buildImgHtml(loadedImageUri);
               }
               appendImageBox(insertImg);
@@ -51,8 +57,9 @@ $(document).on('turbolinks:load', function() {
             fileReader.readAsDataURL(file);
           })
         })
-        if (count_images + files.length == 10){//写真が10枚の時はファイルフィールドを隠す
-          $('.listing-upload__drag-box').hide();
+        // 初期で表示されている画像とアップロードされた画像の枚数が10枚の場合
+        if (count_images + files.length == 10){
+          $('.listing-upload__drag-box').hide(); // アップロードフィールドを隠す
         }
       }
     })
@@ -60,11 +67,12 @@ $(document).on('turbolinks:load', function() {
       e.preventDefault();
       $('#productForm').submit();
     })
+    // リセットプレビューが押された時
     $('.preview-reset').on('click', function(){
-      $('.image-box-new').remove();
-      $('#photo_upload').val("");
-      if ($('.image-box:visible').length != 10){
-        $('.listing-upload__drag-box').show();
+      $('.image-box-new').remove(); // プレビューをリセット
+      $('#photo_upload').val(""); // アップロードされた中身をリセット
+      if ($('.image-box:visible').length != 10){ // 初期で表示中の画像枚数が10枚でない時
+        $('.listing-upload__drag-box').show(); // アップロードフィールドを表示する
       }
     })
   })
