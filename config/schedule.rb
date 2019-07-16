99dbc9ff7c29327e4f87bcb68f4c65ccf73a7e7b
nagisa-miura <nagisaue@icloud.com>
2019-07-16 16:16:38 +0900

    テスト

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,8 @@
-env :PATH, ENV['PATH']
+env :GEM_PATH, ENV['GEM_PATH']
+# env :PATH, ENV['PATH']
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 set :output, '/var/www/freemarket_sample_53b/shared/log/crontab.log'
-job_type :runner, "export PATH=\"$HOME/.rbenv/shims:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rails runner :task ：output"
+job_type :runner, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rails runner :task ：output"
 
 every 1.day at: '11:00 am' do
   runner "ActiveStorage::Blob.unattached.find_each(&:purge_later)"
