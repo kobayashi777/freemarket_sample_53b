6db1b15c2264e522272acba0f2cffb3de2488ca1
nagisa-miura <nagisaue@icloud.com>
2019-07-18 10:58:04 +0900

    定期処理保留

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,6 @@
 # env :PATH, ENV['PATH']
-require File.expand_path(File.dirname(__FILE__) + "/environment")
+# require File.expand_path(File.dirname(__FILE__) + "/environment")
 # set :output, '/var/www/freemarket_sample_53b/shared/log/crontab.log'
-# job_type :runner, "cd :path && shims/bundle runner -e :environment ':task' :output"
 
 # every 1.day at: '11:00 am' do
 #   runner "ActiveStorage::Blob.unattached.find_each(&:purge_later)"
